1158a48349da4dad821dbda66f165a0457d3dc44
Jack O'Hara <jack@jackohara.io>
2022-03-11 17:26:56 +0000

    Finished cards

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,17 @@ export default function Document() {
                     rel="stylesheet"
                 />
                 <link rel="shortcut icon" href="/whole-banner-square.svg" />
+                <link
+                    rel="stylesheet"
+                    type="text/css"
+                    charSet="UTF-8"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+                />
+                <link
+                    rel="stylesheet"
+                    type="text/css"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+                />
             </Head>
             <body>
                 <Main />
